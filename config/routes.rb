a2883bb337e0b954fc87ac6779c1d97d5cde5213
Sean Gaffney <seangaffney@gmail.com>
2011-01-21 14:32:37 -0500

    Added "/map" route

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,8 @@ CoworkgreenvilleCom::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  
+  match "/map" => redirect("http://cl.ly/2PAQ") 
 
   # See how all your routes lay out with "rake routes"
 
